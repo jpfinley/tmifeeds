9aa262631c8c9a35ef21601938e6da0c2036f566
Joshua Davison <josh@stringbot.com>
2009-01-11 20:23:27 -0600

    refactoring common features into a superclass

---
+++ js/tmiFeeds.js
@@ -0,0 +1,93 @@
+var tmiFeeds = Class.create({
+  element: '',
+  items: [],
+  fetch: function(js_url, element) {
+    this.element = element;
+    Event.observe(window, 'load', function() {
+       var twitter_JSON = document.createElement("script");
+       twitter_JSON.type="text/javascript"
+       twitter_JSON.src=js_url
+       document.getElementsByTagName("head")[0].appendChild(twitter_JSON);
+    });
+  },
+  getItems: function(feed) { return feed; },
+  renderFeed: function(feed) {
+    var items = this.getItems(feed);
+    var html = [];
+    html.push("<ul>");
+    items.each(function(item) {
+      html.push("<li>");
+      html.push(this.renderItem(item));
+      html.push("</li>");
+    }.bind(this));
+    html.push("</ul>");
+    $(this.element).insert(html.join(''));    
+  },
+  renderItem: function(item) {
+    return item.toString();
+  }
+});
+
+// Twitter
+var tweets = new tmiFeeds();
+tweets.linkToStatus = function(userName, statusId, text) {
+  return "<a href='http://twitter.com/"+userName+"/status/"+statusId+"'>"+text+"</a>"
+}
+tweets.replyToLink = function(userName, statusId) {
+  return linkToStatus(userName, statusId, "in reply to "+userName);
+}
+tweets.renderItem = function(item) { return item.text; };
+tweets.fetch("http://twitter.com/statuses/user_timeline/stringbot.json?callback=tweets.renderFeed&count=5", "tweets");
+
+
+// Google Reader
+var greader = new tmiFeeds();
+
+greader.getItems = function(feed) { return feed.items; }
+greader.renderItem = function(item) { return item.title; };
+greader.fetch("http://www.google.com/reader/public/javascript/user/01250286733713903147/state/com.google/broadcast?n=5&callback=greader.renderFeed", "greader");
+
+/* JavaScript extensions */
+
+Object.extend(Date.prototype, {
+  toRelativeTimeString: function() {
+    var delta = parseInt((new Date().getTime() - this) / 1000);
+    if(delta < 60) {
+      return 'less than a minute ago';
+    } else if(delta < 120) {
+      return 'about a minute ago';
+    } else if(delta < (45*60)) {
+      return (parseInt(delta / 60)).toString() + ' minutes ago';
+    } else if(delta < (90*60)) {
+      return 'about an hour ago';
+    } else if(delta < (24*60*60)) {
+      var n = parseInt(delta / 3600);
+      dur = n == 1 ? 'an hour' : n.toString() + ' hours';
+      return 'about ' + dur + ' ago';
+    } else if(delta < (48*60*60)) {
+      return '1 day ago';
+    } else {
+      return (parseInt(delta / 86400)).toString() + ' days ago';
+    }
+  }
+});
+
+Object.extend(String.prototype, {
+  parseURL: function() {
+    return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/, function(url) {
+      return url.link(url);
+    });
+  },
+  parseUsername: function() {
+    return this.replace(/[@]+[A-Za-z0-9-_]+/, function(u) {
+      var username = u.replace("@","")
+      return u.link("http://twitter.com/"+username);
+    });
+  },
+  parseHashtag: function() {
+    return this.replace(/[#]+[A-Za-z0-9-_]+/, function(t) {
+      var tag = t.replace("#","%23")
+      return t.link("http://search.twitter.com/search?q="+tag);
+    });
+  }
+});
